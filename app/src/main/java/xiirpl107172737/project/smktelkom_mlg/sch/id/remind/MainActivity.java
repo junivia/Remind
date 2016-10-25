b4de828c54ae607ff154e8b1b329729e05f72d8c
Fadhil Ikhsan <f.ikhsanta@gmail.com>
2016-10-25 11:32:03 +0700

    Tampilan awal v.1

--- app/src/main/java/xiirpl107172737/project/smktelkom_mlg/sch/id/remind/MainActivity.java
+++ app/src/main/java/xiirpl107172737/project/smktelkom_mlg/sch/id/remind/MainActivity.java
@@ -148,11 +148,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "HARI INI";
                 case 1:
-                    return "SECTION 2";
+                    return "IBADAH";
                 case 2:
-                    return "SECTION 3";
+                    return "DO'A";
             }
             return null;
         }
