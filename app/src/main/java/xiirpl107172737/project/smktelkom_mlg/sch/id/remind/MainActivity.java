e6602c88c9fbacf702c25309d5db0af220b64eb2
Alherani Junivia <junivia1999@gmail.com>
2016-10-31 08:21:39 +0700

    Update layout

--- app/src/main/java/xiirpl107172737/project/smktelkom_mlg/sch/id/remind/MainActivity.java
+++ app/src/main/java/xiirpl107172737/project/smktelkom_mlg/sch/id/remind/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.islam);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
