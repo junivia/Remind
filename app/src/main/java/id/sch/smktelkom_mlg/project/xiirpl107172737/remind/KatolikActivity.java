f94116e029837456aafbc8a08de0997329d7b87f
Alherani Junivia <junivia1999@gmail.com>
2016-11-20 19:09:17 +0700

    Data doa islam, katolik dan update tab

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl107172737/remind/KatolikActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl107172737/remind/KatolikActivity.java
@@ -10,12 +10,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class KatolikActivity extends AppCompatActivity {
 
@@ -123,37 +120,6 @@ public class KatolikActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_katolik, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -167,9 +133,19 @@ public class KatolikActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 0:
+                    reminder_katolik tab1 = new reminder_katolik();
+                    return tab1;
+                case 1:
+                    today_katolik tab2 = new today_katolik();
+                    return tab2;
+                case 2:
+                    doa_katolik tab3 = new doa_katolik();
+                    return tab3;
+                default:
+                    return null;
+            }
         }
 
         @Override
@@ -189,6 +165,7 @@ public class KatolikActivity extends AppCompatActivity {
                     return "";
             }
             return null;
+
         }
     }
 }
